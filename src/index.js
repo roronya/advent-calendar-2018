e0aded694b469b5948889b97c6d24062ff124e11
roronya <roronya628@gmail.com>
2018-12-12 11:38:37 +0900

    use redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+import SearchCondition from "./SearchCondition";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const searchConditionReducer = (state = new SearchCondition(), action) => {
+  switch (action.type) {
+    case "SEARCH_SALON":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+const store = createStore(
+  combineReducers({
+    searchCondition: searchConditionReducer
+  })
+);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
